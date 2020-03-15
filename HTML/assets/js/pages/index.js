56be316eede82213588b93657163a6874d846b96
harshmoolani <52037269+harshmoolani@users.noreply.github.com>
2020-03-15 08:25:49 -0500

    updates

---
+++ HTML/assets/js/pages/index.js
@@ -0,0 +1,74 @@
+﻿$(function () {
+	"use strict";       
+    new Chart(document.getElementById("line_chart").getContext("2d"), getChartJs('line'));
+    new Chart(document.getElementById("bar_chart").getContext("2d"), getChartJs('bar'));   
+
+    $(".sparkline").each(function () {
+        var $this = $(this);
+        $this.sparkline('html', $this.data());
+    });
+
+    $('.sparkline-pie').sparkline('html', {
+        type: 'pie',
+        offset: 90,
+        width: '150px',
+        height: '150px',
+        sliceColors: ['#E91E63', '#00BCD4', '#FFC107']
+    })
+
+});
+function getChartJs(type) {
+    var config = null;
+
+    if (type === 'line') {
+        config = {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    borderColor: 'rgba(0, 188, 212, 0.75)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                    pointBorderWidth: 1
+                }, {
+                        label: "My Second dataset",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        borderColor: 'rgba(233, 30, 99, 0.75)',
+                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
+                        pointBorderColor: 'rgba(233, 30, 99, 0)',
+                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                        pointBorderWidth: 1
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'bar') {
+        config = {
+            type: 'bar',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    backgroundColor: 'rgba(0, 188, 212, 0.8)'
+                }, {
+                        label: "My Second dataset",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        backgroundColor: 'rgba(233, 30, 99, 0.8)'
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }    
+    return config;
+}
